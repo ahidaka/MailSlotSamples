b2be5aa39b69c18e9c7eaad3ee27f2067e21f114
ahidaka <hidaka@devdrv.co.jp>
2022-01-04 23:55:26 +0900

    Initial commit for MSReceiveClient and MSSendServer

---
+++ MSSendServer/MSSendServer/MSSendServer.cpp
@@ -0,0 +1,89 @@
+﻿// MSSendServer.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+
+HANDLE hSlot;
+LPCTSTR SlotName = TEXT("\\\\.\\mailslot\\sample_mailslot");
+
+BOOL WINAPI MakeSlot(LPCTSTR lpszSlotName)
+{
+    hSlot = CreateMailslot(lpszSlotName,
+        0,                             // no maximum message size 
+        MAILSLOT_WAIT_FOREVER,         // no time-out for operations 
+        (LPSECURITY_ATTRIBUTES)NULL); // default security
+
+    if (hSlot == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateMailslot failed with %d\n", GetLastError());
+        return FALSE;
+    }
+    else printf("Mailslot created successfully.\n");
+    return TRUE;
+}
+
+BOOL WriteSlot(HANDLE hSlot, LPCTSTR lpszMessage)
+{
+    BOOL fResult;
+    DWORD cbWritten;
+
+    fResult = WriteFile(hSlot,
+        lpszMessage,
+        (DWORD)(lstrlen(lpszMessage) + 1) * sizeof(TCHAR),
+        &cbWritten,
+        (LPOVERLAPPED)NULL);
+
+    if (!fResult)
+    {
+        printf("WriteFile failed with %d.\n", GetLastError());
+        return FALSE;
+    }
+
+    printf("Slot written to successfully.\n");
+
+    return TRUE;
+}
+
+
+int main()
+{
+    HANDLE hFile;
+
+    std::cout << "Hello Mail Slot!\n";
+
+#if 0
+    BOOL success = MakeSlot(SlotName);
+
+    if (!success) {
+        printf("MakeSlot error\n");
+        return 1;
+    }
+#endif
+
+    hFile = CreateFile(SlotName,
+        GENERIC_WRITE,
+        FILE_SHARE_READ,
+        (LPSECURITY_ATTRIBUTES)NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        (HANDLE)NULL);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateFile failed with %d.\n", GetLastError());
+        return FALSE;
+    }
+
+    WriteSlot(hFile, TEXT("Message one for mailslot."));
+    WriteSlot(hFile, TEXT("Message two for mailslot."));
+
+    Sleep(5000);
+
+    WriteSlot(hFile, TEXT("Message three for mailslot."));
+
+    CloseHandle(hFile);
+
+    return TRUE;
+}
